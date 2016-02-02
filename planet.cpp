b4eb2193fd0d5eefa16b6cf2b102758bf2f43688
Kevin Wells <darkoppressor@gmail.com>
2016-02-02 05:07:16 -0600

    Add optional collision circle to Planet

--- planet.cpp
+++ planet.cpp
@@ -3,11 +3,10 @@
 /* See the file docs/LICENSE.txt for the full license text. */
 
 #include "planet.h"
+#include "game_options.h"
 
 #include <game_manager.h>
-///QQQ includes
-///#include <render.h>
-///
+#include <render.h>
 
 using namespace std;
 
@@ -31,8 +30,8 @@ void Planet::render(){
     if(Collision::check_circ_rect(circle*Game_Manager::camera_zoom,Game_Manager::camera)){
         sprite.render((circle.x-circle.r)*Game_Manager::camera_zoom-Game_Manager::camera.x,(circle.y-circle.r)*Game_Manager::camera_zoom-Game_Manager::camera.y);
 
-        ///QQQ render collision circle
-        ///Render::render_circle(circle.x*Game_Manager::camera_zoom-Game_Manager::camera.x,circle.y*Game_Manager::camera_zoom-Game_Manager::camera.y,circle.r,0.25,"red");
-        ///
+        if(Game_Options::show_collision_outlines){
+            Render::render_circle_empty(circle.x*Game_Manager::camera_zoom-Game_Manager::camera.x,circle.y*Game_Manager::camera_zoom-Game_Manager::camera.y,circle.r,1.0,"red");
+        }
     }
 }
