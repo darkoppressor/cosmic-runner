dee017bc99caf476b5eddd226a48ce164504ef1e
Kevin Wells <darkoppressor@gmail.com>
2019-08-28 05:28:02 -0400

    Update version series

--- version.cpp
+++ version.cpp
@@ -36,5 +36,5 @@ string Engine_Version::get_status () {
 
 void Engine_Version::populate_version_series (vector<Version_Series>& version_series) {
     version_series.push_back(Version_Series("0.0.1", "0.0.1"));
-    version_series.push_back(Version_Series("0.1.0", "0.2.6"));
+    version_series.push_back(Version_Series("0.1.0", "0.2.7"));
 }
